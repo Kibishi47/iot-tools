786b673bfd9a45bd765d633034acdc7e55596fdf
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-11-29 16:14:27 +0100

    feat: base code structure and test rover OK

---
+++ IOTTools/Robots/Models/RobotType.swift
@@ -0,0 +1,23 @@
+//
+//  RobotType.swift
+//  IOTTools
+//
+//  Created by Emmanuel Moulin on 29/11/2025.
+//
+
+import Foundation
+import Synchrosphere
+
+enum RobotType {
+    case rover
+    case sphero
+    
+    func toSynchroSphere() -> SyncsDeviceSelector {
+        return switch (self) {
+            case .rover:
+                .anyRVR
+            case .sphero:
+                .anyMini
+        }
+    }
+}
