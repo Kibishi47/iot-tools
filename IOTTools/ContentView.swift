3679f2654a303fb3d605cd60ea769c43f8260b49
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2025-11-29 11:08:01 +0100

    Initial commit: Add Swift project

---
+++ IOTTools/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOTTools
+//
+//  Created by Emmanuel Moulin on 29/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
